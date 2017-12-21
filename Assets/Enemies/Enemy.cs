b06e68b4f1d5ceaf2da74d1fb58f6c51a3016b4f
b <biro123@ntlworld.com>
2017-12-21 01:36:16 +0000

    Added enemy formations.. ie shield-walls and changed the standard enemy to Levy and Veteran.

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -2,17 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.ThirdPerson;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour {
 
     [SerializeField] float maxHealthPoints = 100f;
     [SerializeField] float aggroDistance = 10f;
 
+    [Tooltip("Can Lead ShieldWalls")]
+    [SerializeField] bool isLeader = false;
+
+    [Tooltip("Aggro distance while in formation")]
+    [SerializeField] float formationAggroDistance = 3f;
+
+    [Tooltip("Distance from enemy to reform")]
+    [SerializeField] float reformDistance = 10f;
+
+    [Tooltip("Stop Distance while reforming")]
+    [SerializeField] float reformStopDistance = 0.5f;
+
+
     private float currentHealthPoints = 100f;
-    private ThirdPersonCharacter thirdPersonCharacter = null;
+    private float startingStopDistance;
     private Transform startPosition = null;
+    private Transform formationPosition = null;
     private AICharacterControl aICharacterControl = null;
+    private NavMeshAgent navMeshAgent;
     private Player player = null;
+    private bool inFormation = false;
 
     
     public float healthAsPercentage
@@ -23,26 +40,55 @@ public class Enemy : MonoBehaviour {
         }
     }
 
+    public void SetFormationPos(Transform position)
+    {
+        formationPosition = position;
+    }
+
     private void Start()
     {
         startPosition = this.transform;
         player = FindObjectOfType<Player>();
         aICharacterControl = GetComponent<AICharacterControl>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        startingStopDistance = navMeshAgent.stoppingDistance;
     }
 
     private void Update()
     {
         if (!player) { return; }
 
+        if (formationPosition != null )
+        {
+            aggroDistance = formationAggroDistance;               
+        }
+        else
+        {
+            // TODO reset aggro distance when losing formation
+        }
+
+        // TODO - needs a reform method for when out of range
+        // TODO - would like to be able to target friendly NPC
+        
         var distanceToPlayer = this.transform.position - player.transform.position;
         if (distanceToPlayer.magnitude <= aggroDistance)
         {
+            navMeshAgent.stoppingDistance = startingStopDistance;     // stopping distance for attacking
             aICharacterControl.SetTarget(player.transform);
         }
         else
-        {  // TODO - not working as transform moves with enemy
-            aICharacterControl.SetTarget(startPosition);  
+        {  
+            if (formationPosition != null)
+            {
+                navMeshAgent.stoppingDistance = reformStopDistance;   // small stopping distance to keep formation
+                aICharacterControl.SetTarget(formationPosition);
+            }
+            else
+            {           
+                aICharacterControl.SetTarget(startPosition);
+            }            
         }
+
     }
 
 }
---
+++ Assets/Enemies/Formation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class Formation : MonoBehaviour {
+
+    [SerializeField] GameObject row1Enemy;
+    [SerializeField] GameObject leaderPrefab;
+    
+    private Transform[] troopPositions;
+    private GameObject leader;
+
+	// Use this for initialization
+	void Start () {
+
+        troopPositions = GetComponentsInChildren<Transform>();
+        foreach (Transform troopPosition in troopPositions)
+        {
+            if (troopPosition.position == this.transform.position)
+            {
+                leader = Instantiate(leaderPrefab, troopPosition.position, this.transform.rotation, transform.parent);
+                leader.GetComponent<Enemy>().SetFormationPos(troopPosition);
+            }
+            else
+            {
+                GameObject enemy = Instantiate(row1Enemy, troopPosition.position, this.transform.rotation, transform.parent);
+                enemy.GetComponent<Enemy>().SetFormationPos(troopPosition);
+                
+                enemy.GetComponent<AICharacterControl>().SetTarget(troopPosition);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = leader.transform.position;
+        transform.rotation = leader.transform.rotation;
+        //transform.localScale = warlord.localScale;
+	}
+}
