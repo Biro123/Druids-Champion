8fd7a33ee986600183004aecc6f53639e9381780
b <biro123@ntlworld.com>
2018-01-24 01:33:55 +0000

    Set up storage for special ability config data

---
+++ Assets/_Characters/Special Abilities/Aimed Shot/AimedShotBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace RPG.Characters
+{
+    public class AimedShotBehaviour : MonoBehaviour, ISpecialAbility 
+    {
+        AimedShotConfig config;
+
+        public void SetConfig(AimedShotConfig configToSet)
+        {
+            this.config = configToSet;
+        }
+
+        public void Use()
+        {
+
+        }
+
+    }
+}
---
+++ Assets/_Characters/Special Abilities/Aimed Shot/AimedShotConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Characters
+{
+    [CreateAssetMenu(menuName = ("RPG/Special Ability/Aimed Shot"))]
+    public class AimedShotConfig : SpecialAbilityConfig
+    {
+        [Header("Aimed Shot Specific")]
+        [SerializeField] float extraDamage = 10f;
+
+        public override ISpecialAbility AddComponent(GameObject gameObjectToAttachTo)
+        {
+            var behaviourComponent = gameObjectToAttachTo.AddComponent<AimedShotBehaviour>();
+            behaviourComponent.SetConfig(this);
+            return behaviourComponent;
+        }
+    }
+}
