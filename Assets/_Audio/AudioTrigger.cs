a2f66888dbcc207e8daa5f587d570199d22ab4d8
b <biro123@ntlworld.com>
2018-02-06 01:59:32 +0000

    fix to audio triggers interfering with raycaster

--- Assets/_Audio/AudioTrigger.cs
+++ Assets/_Audio/AudioTrigger.cs
@@ -1,30 +1,28 @@
 ﻿using UnityEngine;
+using RPG.Characters;
 
 public class AudioTrigger : MonoBehaviour
 {
     [SerializeField] AudioClip clip;
-    [SerializeField] int layerFilter = 0;
     [SerializeField] float triggerRadius = 5f;
     [SerializeField] bool isOneTimeOnly = true;
 
-    [SerializeField] bool hasPlayed = false;
+    bool hasPlayed = false;
     AudioSource audioSource;
+    GameObject player;
 
     void Start()
     {
         audioSource = gameObject.AddComponent<AudioSource>();
         audioSource.playOnAwake = false;
         audioSource.clip = clip;
-
-        SphereCollider sphereCollider = gameObject.AddComponent<SphereCollider>();
-        sphereCollider.isTrigger = true;
-        sphereCollider.radius = triggerRadius;
-        gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");   // so audio collider does not break camera raycaster
+        player = FindObjectOfType<PlayerControl>().gameObject;
     }
 
-    void OnTriggerEnter(Collider other)
+    private void Update()
     {
-        if (other.gameObject.layer == layerFilter)
+        var distanceToPlayer = Vector3.Magnitude(transform.position - player.transform.position);
+        if(distanceToPlayer <= triggerRadius)
         {
             RequestPlayAudioClip();
         }
